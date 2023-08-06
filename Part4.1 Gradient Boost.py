18151155285a20408da3a4e1dbdc1ab317e190db
nathanahearn <31374612+nathanahearn@users.noreply.github.com>
2023-08-06 18:54:18 -0300

    Update and rename Part4.1 Linear Regression (cont).py to Part4.1 Gradient Boost.py

---
+++ Part4.1 Gradient Boost.py
@@ -0,0 +1,36 @@
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.impute import SimpleImputer
+
+# (With Part 4 code before this)
+# Gradient Boost
+
+X = player_data_df[['DY-1 PPG', 'DY-1 Shots per game', 'DY PPG', 'DY Shots per game']]
+y = player_data_df['DY+1 PPG']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Handle missing values using SimpleImputer
+imputer = SimpleImputer(strategy='mean')
+X_train_imputed = pd.DataFrame(imputer.fit_transform(X_train), columns=X_train.columns)
+X_test_imputed = pd.DataFrame(imputer.transform(X_test), columns=X_test.columns)
+
+# Create and fit the Gradient Boosting Regressor
+gb_regressor = GradientBoostingRegressor()
+gb_regressor.fit(X_train_imputed, y_train)
+
+# Make predictions on the test set
+y_pred_gb = gb_regressor.predict(X_test_imputed)
+
+# Evaluate the model
+mse_gb = mean_squared_error(y_test, y_pred_gb)
+rmse_gb = mean_squared_error(y_test, y_pred_gb, squared=False)
+mae_gb = mean_absolute_error(y_test, y_pred_gb)
+r2_gb = r2_score(y_test, y_pred_gb)
+
+print("Gradient Boosting Regressor Metrics:")
+print("Mean Squared Error (MSE):", mse_gb)
+print("Root Mean Squared Error (RMSE):", rmse_gb)
+print("Mean Absolute Error (MAE):", mae_gb)
